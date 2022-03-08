c2871a624213b7063617ce0e367ec3d7f79592b1
KenanNagiyev <788lnmp@code.edu.az>
2022-03-08 21:15:52 +0400

    new proekt

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine(VowelLetter(text));
+        }
+            static int VowelLetter(string text)
+            {
+                int count = 0;
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] == 'a' || text[i] == 'e' || text[i] == 'o' || text[i] == 'i' || text[i] == 'u')
+                    {
+                        count++;
+                    }
+                }
+                return count;            
+        }
+    }
+}
